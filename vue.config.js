9cde8970e0d6767fdc51144b5a3f315fde472288
cinquemb <1519017+cinquemb@users.noreply.github.com>
2024-07-30 03:20:22 -0400

    fix: vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/rethink-frontend/'
+    ? '/simple-foundance-deals/'
     : '/',
     configureWebpack: {
         module: {
